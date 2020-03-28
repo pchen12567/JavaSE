f24d2dd5bb8a3299d196553d6435f699a4825340
pchen12567 <pchen12@stevens.edu>
2020-03-28 17:44:55 +0800

    Upload day03

---
+++ day03_流程控制语句/Demo17LoopHourAndMinute.java
@@ -0,0 +1,9 @@
+public class Demo17LoopHourAndMinute {
+	public static void main(String[] args) {
+		for (int hour = 0; hour < 24; hour++) {
+			for (int minute = 0; minute < 60; minute++) {
+				System.out.println(hour + "点" + minute + "分");
+			}
+		}
+	}
+}
