57f5b7c49d6dd7ba2f18b88762d5e07131449832
pchen12567 <pchen12@stevens.edu>
2020-04-05 22:17:33 +0800

    Upload day12 Object类以及常用API

---
+++ day12_Object类及常用API/src/com/day12/demo01/Demo01ToString.java
@@ -0,0 +1,46 @@
+package com.day12.demo01;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+    java.lang.Object
+    类 Object 是类层次结构的根(父)类。
+    每个类(Person,Student...)都使用 Object 作为超(父)类。
+    所有对象（包括数组）都实现这个类的方法。
+ */
+public class Demo01ToString {
+
+    public static void main(String[] args) {
+        /*
+            Person类默认继承了Object类,所以可以使用Object类中的toString方法
+            String toString() 返回该对象的字符串表示。
+        */
+        Person p = new Person("张三", 18);
+        String s = p.toString();
+        System.out.println(s);
+
+        //直接打印对象的名字,其实就是调用对象的toString  p=p.toString();
+        System.out.println(p);
+
+        /*
+            看一个类是否重写了toString,直接打印这个类的对象即可,
+            如果没有重写toString方法那么打印的是对象的地址值(默认）
+            如果重写toString方法，那么久按照重写的方式打印
+        */
+        Random r = new Random();
+        System.out.println(r); // 没有重写toString方法
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc); // 重写toString方法
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        System.out.println(list); // 重写toString方法
+
+
+
+    }
+}
---
+++ day12_Object类及常用API/src/com/day12/demo02/Demo02Date.java
@@ -0,0 +1,42 @@
+package com.day12.demo02;
+
+import java.util.Date;
+
+public class Demo02Date {
+
+    public static void main(String[] args) {
+        demo03();
+    }
+
+    /*
+        long getTime() 把日期转换为毫秒值(相当于System.currentTimeMillis()方法)
+          返回自 1970 年 1 月 1 日 00:00:00 GMT 以来此 Date 对象表示的毫秒数。
+     */
+    private static void demo03() {
+        Date data = new Date();
+        long time = data.getTime();
+        System.out.println(time); // 1586074692012
+    }
+
+    /*
+        Date类的带参数构造方法
+        Date(long date) :传递毫秒值,把毫秒值转换为Date日期
+     */
+    private static void  demo02() {
+        Date d1 = new Date(0L);
+        System.out.println(d1); // Thu Jan 01 08:00:00 CST 1970
+
+        Date d2 = new Date(158111111111L);
+        System.out.println(d2); // Sun Jan 05 07:45:11 CST 1975
+    }
+
+    /*
+        Date类的空参数构造方法
+        Date() 获取当前系统的日期和时间
+     */
+    private static void demo01() {
+        Date date = new Date();
+        System.out.println(date); // Sun Apr 05 16:13:45 CST 2020
+    }
+
+}
